44ecb0d03cbcc0609888d5eb9137bc679d9fcd83
Harry Bagdi <harrybagdi@gmail.com>
2023-08-22 09:38:54 -0700

    bump to 3.4 and some debugging

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM kong/kong-gateway:3.3
+FROM kong/kong-gateway:3.4
 USER root
 COPY kong-fly.sh /kong-fly.sh
 ENTRYPOINT /kong-fly.sh
--- kong-fly.sh
+++ kong-fly.sh
@@ -2,8 +2,12 @@
 
 echo "configuring mTLS cert and key..."
 mkdir /certs
-echo $KONG_DP_CERT | base64 -d > /certs/dp-cert
-echo $KONG_DP_KEY  | base64 -d > /certs/dp-key
+echo "DP cert is---"
+echo -n $KONG_DP_CERT
+echo "writing dp-cert to disk"
+echo -n "$KONG_DP_CERT" | base64 -d > /certs/dp-cert
+echo "writing dp-key to disk"
+echo -n "$KONG_DP_KEY"  | base64 -d > /certs/dp-key
 
 echo "running kong's entrypoint.sh..."
 
